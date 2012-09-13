ea40a74fd0ba160502195897641064a4d68afacb
MartinCattaneo <ktta_182@hotmail.com>
2012-09-13 17:54:59 -0300

    /modulos/comandos/seguridad.sh -> 	*Hice que funcione las restricciones de comandos con tuberias(ej: pwd> pwd > etc)

--- modulos/comandos/seguridad.sh
+++ modulos/comandos/seguridad.sh
@@ -1,16 +1,16 @@
 
 #!/bin/bash
-#TODO:No funcionan los comandos con tuberias
 
 #$1 ->Tipo de operacion
 #$2 ->String del comando (parametro)
 
 CONFG_FILE="/home/utnso/TP1/modulos/comandos/seguridad.conf"
+TUBERIAS="<>|&"
 
 if [ $1 = "procesar" ];then
   while read COMANDO_RESTRINGIDO
   do
-  	STR_FILTRADO=`echo " $2 " | grep -e " $COMANDO_RESTRINGIDO "`
+  	STR_FILTRADO=`echo " $2 " | grep -E "( $COMANDO_RESTRINGIDO | $COMANDO_RESTRINGIDO[$TUBERIAS]|[$TUBERIAS]$COMANDO_RESTRINGIDO |[$TUBERIAS]$CMDR[$TUBERIAS])"`
   	if [ -n "$STR_FILTRADO" ];then #Contiene un comando restringido
 		echo "Error: El comando $COMANDO_RESTRINGIDO esta restringido."
 		exit 1  	
