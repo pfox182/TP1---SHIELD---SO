ec5b3ee37f3702f74f4240df56b1408e8f7632b1
Martin de la Llave <delallave.martin@gmail.com>
2012-09-17 23:40:37 -0300

    Se agrego la operacion informacion al modulo de auditoria.

--- modulos/comandos/auditoria.sh
+++ modulos/comandos/auditoria.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-OUTPUT_FILE="/home/utnso/output_auditoria"
+source /home/utnso/TP1---SHIELD---SO/modulos/comandos/auditoria.conf
 
-if [ $1 = "procesar" ];then
+OUTPUT_FILE="/home/utnso/output_auditoria"
+ARCHIVO_LOG_LOCAL="/home/utnso/TP1---SHIELD---SO/modulos/comandos/auditoria.conf"
+
+if [ "$1" = "procesar" ];then
   if [ -s "$OUTPUT_FILE" ]; then
     echo $2 >> $OUTPUT_FILE
   else
     echo $2 > $OUTPUT_FILE
   fi
-  exit 0 
 fi
+
+if [ "$1" = "informacion" ];then
+	TAMANO_ARCHIVO_LOG=`stat -c %s $ARCHIVO_LOG_LOCAL` #Obtengo el tamaño del archivo de log local.	
+	echo "Tamaño maximo del archivo local de log: $TAMANO_MAXIMO% , IP del servidor remoto de logueo: $IP_SERVIDOR_REMOTO% , tamaño del archivo de log local: $TAMANO_ARCHIVO_LOG%."
+
+fi
+
+exit 0 
 
 
