6b2a14f2824fd3a34a3183dff4866554d9bb5a04
utnso <utnso@utnso-vm>
2012-09-20 11:55:53 -0300

    subo el modulo de control_sesiones que controla la cantidad de sesiones por usuario.

---
+++ modulos/comandos/control_sesiones.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+#Cargo archivo de configuracion
+. /home/utnso/TP1---SHIELD---SO/modulos/comandos/control_sesiones.conf
+
+#Cantidad de sesiones abiertas por un mismo usuario
+CANT_SESIONES=`ps aux | grep bash | grep -v grep | grep $USER | wc -l`
+
+#CANT_SESIONES=$(who | grep $(whoami) | wc -l)
+
+
+#Comparo la cantidad max permitida con la cant actual de sesiones
+if [ "$1" = "iniciar" ];then
+     if [ $CANT_MAX_SESIONES -ge $CANT_SESIONES ];then
+	echo "La cantidad actual de sesiones abiertas es: $CANT_SESIONES"
+     else 
+	echo "Cantidad maxima de sesiones superadas"
+     fi	
+fi 
+
+#Informo el estado actual de la configuracion y de las sesiones activas
+if [ "$1" = "informacion" ];then
+	echo "La cantidad actual de sesiones abiertas es: $CANT_SESIONES"
+	echo "La cantidad maxima de sesiones permitidas por usuario es: $CANT_MAX_SESIONES"
+fi
+
+
+exit 0
