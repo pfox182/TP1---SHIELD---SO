9c0fa956f857350df0833c22f9653ea9b67dca88
MartinCattaneo <ktta_182@hotmail.com>
2012-09-20 20:27:54 -0300

    nucleo.sh -> 	*Se cambiaron los source a . (Ya que esa es la manera de incluir codigo en bash) 	*Se agregaron capturas de señales de terminacion 	*Se corrigio el bug de los procesos en 2° plano que seguian corriendo 	*Se incluyo la funcion terminar_procesos_en_segundo_plano.sh 	! -> Se comento la inicialisacion de los modulos, ya que hay un bug(pide permiso para sobreescribir, mañana veo como lo arreglo)
    includes/terminar_procesos_en_segundo_plano.sh ->
    	*Busca los prosesos de la terminal del usuario que lo invoca y los termina.

--- nucleo.sh
+++ nucleo.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
-source /home/utnso/TP1---SHIELD---SO/includes/prompt.sh
+. /home/utnso/TP1---SHIELD---SO/includes/prompt.sh
 #Incluciones de datos
-source /home/utnso/TP1---SHIELD---SO/nucleo.conf
+. /home/utnso/TP1---SHIELD---SO/nucleo.conf
 
 #Incluciones de funcionalidad
-source /home/utnso/TP1---SHIELD---SO/includes/validar_comando.sh
-source /home/utnso/TP1---SHIELD---SO/includes/es_un_built_in.sh
-source /home/utnso/TP1---SHIELD---SO/includes/ejecutar_built_in.sh
-source /home/utnso/TP1---SHIELD---SO/includes/ejecutar_comando.sh
-source /home/utnso/TP1---SHIELD---SO/includes/inicializar_modulos_de_comando.sh
+. /home/utnso/TP1---SHIELD---SO/includes/validar_comando.sh
+. /home/utnso/TP1---SHIELD---SO/includes/es_un_built_in.sh
+. /home/utnso/TP1---SHIELD---SO/includes/ejecutar_built_in.sh
+. /home/utnso/TP1---SHIELD---SO/includes/ejecutar_comando.sh
+. /home/utnso/TP1---SHIELD---SO/includes/inicializar_modulos_de_comando.sh
+. /home/utnso/TP1---SHIELD---SO/includes/terminar_procesos_en_segundo_plano.sh
 
+. /home/utnso/TP1---SHIELD---SO/includes/ejemplo
 #Variables de uso comun
 TERMINAL_DE_LA_SESSION=`tty`
 
 #Iniciar modulos de comando
-inicializar_modulos_de_comando
+#inicializar_modulos_de_comando
 
 #Ejecucion de modulos periodicos en segundo plano
 bash /home/utnso/TP1---SHIELD---SO/includes/ejecutar_modulos_periodicos.sh $TIEMPO_MODULOS_PERIODICOS $TERMINAL_DE_LA_SESSION & 
 
-prompt;read STRING
-
 while [ true ]
 do
-  
   #Capturar señal enviada por algun modulo periodico
-  trap "exit 1" SIGUSR1 
+  trap "terminar_procesos_en_segundo_plano $TERMINAL_DE_LA_SESSION TERM;exit 1" SIGUSR1 
+  
+  #Capturar señales de temrinacion
+  trap "terminar_procesos_en_segundo_plano $TERMINAL_DE_LA_SESSION TERM;echo El programa termino inesperadamente.;exit 1" SIGINT
+  trap "terminar_procesos_en_segundo_plano $TERMINAL_DE_LA_SESSION TERM;echo El programa termino inesperadamente.;exit 1" TERM
+  trap "terminar_procesos_en_segundo_plano $TERMINAL_DE_LA_SESSION TERM;echo El programa termino inesperadamente.;exit 1" KILL
+	
+  #Comenzar a leer comandos
+  prompt;read STRING
 
   es_un_built_in "$STRING"
   if [ $? -eq 0 ];then 
@@ -39,7 +46,6 @@ do
 
   fi	
 
-  prompt;read STRING 
 done
 
 exit 1 #Deberia salir con el built-in salir
