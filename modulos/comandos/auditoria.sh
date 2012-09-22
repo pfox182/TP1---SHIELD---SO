5021e03aaf2cc2c37dce92e1c3603447477427a2
Martin de la Llave <delallave.martin@gmail.com>
2012-09-22 01:36:52 -0300

    controla que no pase el tamaño maximo , y si lo hace muestra un mensaje de error, todavia falta que logue en el servidor remoto.

--- modulos/comandos/auditoria.sh
+++ modulos/comandos/auditoria.sh
@@ -3,14 +3,20 @@
 source /home/utnso/TP1---SHIELD---SO/modulos/comandos/auditoria.conf
 
 OUTPUT_FILE="/home/utnso/output_auditoria"
-ARCHIVO_LOG_LOCAL="/home/utnso/TP1---SHIELD---SO/modulos/comandos/auditoria.conf"
+ARCHIVO_CONFIG_LOCAL="/home/utnso/TP1---SHIELD---SO/modulos/comandos/auditoria.conf"
+ 
 
-if [ "$1" = "procesar" ];then
-  if [ -s "$OUTPUT_FILE" ]; then
-    echo $2 >> $OUTPUT_FILE
-  else
-    echo $2 > $OUTPUT_FILE
-  fi
+if [ "$1" = "procesar" ]; then
+    if [ -s "$OUTPUT_FILE" ]; then
+      TAMANO_ARCHIVO_LOG=`stat -c %s $OUTPUT_FILE` #Obtengo el tamaño del archivo de log local.
+      if [ $TAMANO_ARCHIVO_LOG -gt $TAMANO_MAXIMO ]; then
+        echo "supero el tamaño del log local, se comenzara a loguear en el servidor remoto."
+      else
+        echo $2 >> $OUTPUT_FILE
+      fi
+    else
+       echo $2 > $OUTPUT_FILE
+    fi 
 fi
 
 if [ "$1" = "informacion" ];then
