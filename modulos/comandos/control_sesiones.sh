1410966bb6d56a9c480876ed5330852012ec89f8
utnso <utnso@utnso-vm>
2012-09-26 17:44:03 -0300

    se agrego el case a los modulos trafico_red.sh , control_sesiones.sh

--- modulos/comandos/control_sesiones.sh
+++ modulos/comandos/control_sesiones.sh
@@ -7,23 +7,24 @@
 #Cantidad de sesiones abiertas por un mismo usuario
 CANT_SESIONES=`ps aux | grep nucleo.sh | grep -v grep | grep $USER | wc -l`
 
-#CANT_SESIONES=$(who | grep $(whoami) | wc -l)
-
 
 #Comparo la cantidad max permitida con la cant actual de sesiones
-if [ "$1" = "iniciar" ];then
-     if [ $CANT_MAX_SESIONES -ge $CANT_SESIONES ];then
-	echo "La cantidad actual de sesiones abiertas es: $CANT_SESIONES"
-     else 
-	echo "Cantidad maxima de sesiones superadas"
-     fi	
-fi 
+case "$1" in
+	"iniciar" )
+
+    		if [ $CANT_MAX_SESIONES -ge $CANT_SESIONES ];then
+			echo "La cantidad actual de sesiones abiertas es: $CANT_SESIONES"
+     	 	else 
+			echo "Cantidad maxima de sesiones superadas"
+    	 	fi;;
 
 #Informo el estado actual de la configuracion y de las sesiones activas
-if [ "$1" = "informacion" ];then
-	echo "La cantidad actual de sesiones abiertas es: $CANT_SESIONES"
-	echo "La cantidad maxima de sesiones permitidas por usuario es: $CANT_MAX_SESIONES"
-fi
+	"informacion")
+
+		echo "La cantidad actual de sesiones abiertas es: $CANT_SESIONES"
+ 	 	echo "La cantidad maxima de sesiones permitidas por usuario es: $CANT_MAX_SESIONES"
+		exit 0;;
+esac
 
 
-exit 0
+
--- modulos/periodicos/trafico_red.sh
+++ modulos/periodicos/trafico_red.sh
@@ -6,34 +6,36 @@
 CANT_PAQUETESIP_SAL=$(ifconfig $(ifconfig | grep eth | awk '{print$1}') | grep -m 1 "TX" | awk '{print$2}' | cut -d ":" -f 2)
 
 
-#comparo cantidad maxima de paquetes ip salientes con la cantidad actual.
-if [ $1 = "procesar" ];then
-	if [ $CANT_PAQUETESIP_SAL -ge $CANT_MAX_PAQUETESIP_SAL ];then	
-		#Muestro los sockets activos
-		lsof -i > SOCKETS_ABIERTOS.tmp
-		cat SOCKETS_ABIERTOS.tmp 
-		#busco los sockets activos y elimino sus procesos. 
-		lsof -i -t > IDPROCESOS_SOCKETS.tmp
-		while read line;do
-        		#de los procesos encontrado me fijo a que usuario pertenecen
-			USER_PROCESO=$(ps aux | awk '{print$1,$2}' | grep $line | awk '{print$1}')
-        		#comparo si es el usuario del proceso es igual al usuario del shield
-			if [ $USER_PROCESO = $USER ];then
-                		kill -9 $line
-                		echo "Se elimino el Proceso:$line USER:$USER_PROCESO"
-        		fi;done < IDPROCESOS_SOCKETS.tmp
-		#borro archivos temporal 
-		rm IDPROCESOS_SOCKETS.tmp
-		rm SOCKETS_ABIERTOS.tmp
-	fi
-	exit $?
-fi
+case "$1" in
+	"procesar" )
+		#comparo cantidad maxima de paquetes ip salientes con la cantidad actual.
+		if [ $CANT_PAQUETESIP_SAL -ge $CANT_MAX_PAQUETESIP_SAL ];then	
+			#Muestro los sockets activos
+			lsof -i > SOCKETS_ABIERTOS.tmp
+			cat SOCKETS_ABIERTOS.tmp 
+			#busco los sockets activos y elimino sus procesos. 
+			lsof -i -t > IDPROCESOS_SOCKETS.tmp
+			while read line;do
+        			#de los procesos encontrado me fijo a que usuario pertenecen
+				USER_PROCESO=$(ps aux | awk '{print$1,$2}' | grep $line | awk '{print$1}')
+        			#comparo si es el usuario del proceso es igual al usuario del shield
+				if [ $USER_PROCESO = $USER ];then
+                			kill -9 $line
+                			echo "Se elimino el Proceso:$line USER:$USER_PROCESO"
+        			fi;done < IDPROCESOS_SOCKETS.tmp
+			#borro archivos temporal 
+			rm IDPROCESOS_SOCKETS.tmp
+			rm SOCKETS_ABIERTOS.tmp
+		fi
+		exit $?;;
+
+	#informo el estado actual de paquetes ip salientes y la cantidad maxima permitida.
+	"informacion" )
+		echo "La cantidad actual de paquetes ip salientes es : $CANT_PAQUETESIP_SAL"
+		echo "La cantidad maxima de paquetes ip salientes permitidos por usuario es: $CANT_MAX_PAQUETESIP_SAL"
+ 		exit $?;;
+
+esac
 
 
 
-#informo el estado actual de paquetes ip salientes y la cantidad maxima permitida.
-if [ $1 = "informacion" ];then
-	echo "La cantidad actual de paquetes ip salientes es : $CANT_PAQUETESIP_SAL"
-	echo "La cantidad maxima de paquetes ip salientes permitidos por usuario es: $CANT_MAX_PAQUETESIP_SAL"
- 	exit $?
-fi
