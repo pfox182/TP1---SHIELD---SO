00e0018d26ead503dd1b90b70e4eac048be2cc00
Martin de la Llave <delallave.martin@gmail.com>
2012-10-08 22:11:27 -0300

    Se adapto auditoria.sh para la interfase nueva

--- modulos/comandos/auditoria.sh
+++ modulos/comandos/auditoria.sh
@@ -1,29 +1,51 @@
 #!/bin/bash
 
+#$1 ->Tipo de operacion
+#$2 ->String del comando (parametro)
+
 source /home/utnso/TP1---SHIELD---SO/modulos/comandos/auditoria.conf
 
 OUTPUT_FILE="/home/utnso/output_auditoria"
-ARCHIVO_CONFIG_LOCAL="/home/utnso/TP1---SHIELD---SO/modulos/comandos/auditoria.conf"
- 
+CONFG_FILE="/home/utnso/TP1---SHIELD---SO/modulos/comandos/auditoria.conf"
 
-if [ "$1" = "procesar" ]; then
-    if [ -s "$OUTPUT_FILE" ]; then
-      TAMANO_ARCHIVO_LOG=`stat -c %s $OUTPUT_FILE` #Obtengo el tamaño del archivo de log local.
-      if [ $TAMANO_ARCHIVO_LOG -gt $TAMANO_MAXIMO ]; then
-        echo "supero el tamaño del log local, se comenzara a loguear en el servidor remoto."
-      else
-        echo $2 >> $OUTPUT_FILE
-      fi
-    else
-       echo $2 > $OUTPUT_FILE
-    fi 
-fi
+case $1 in
+	iniciar)
+ 		#Leer el archivo de configuracion y exportar la variable con los valores asignados
+  		export COMANDOS=`cat $CONFG_FILE`
+		#No se le pone exit, porque sino al llamarlo con source corta la ejecucion del script que lo llamo ( ej: nucleo.sh )
+		;;
 
-if [ "$1" = "informacion" ];then
-	TAMANO_ARCHIVO_LOG=`stat -c %s $OUTPUT_FILE` #Obtengo el tamaño del archivo de log local.	
-	echo "Tamaño maximo del archivo local de log: $TAMANO_MAXIMO , IP del servidor remoto de logueo: $IP_SERVIDOR_REMOTO , tamaño del archivo de log local: $TAMANO_ARCHIVO_LOG."
 
-fi
+	procesar)
+  		if [ -s "$OUTPUT_FILE" ]; then
+	          TAMANO_ARCHIVO_LOG=`stat -c %s $OUTPUT_FILE` #Obtengo el tamaño del archivo de log local.
+			if [ $TAMANO_ARCHIVO_LOG -gt $TAMANO_MAXIMO ]; then
+			  echo "supero el tamaño del log local, se comenzara a loguear en el servidor remoto."
+			else
+		       	  echo $2 >> $OUTPUT_FILE
+			fi
+	        else
+	         echo $2 > $OUTPUT_FILE
+	        fi 
+	        exit 0
+	        ;;
+
+	informacion)
+  		TAMANO_ARCHIVO_LOG=`stat -c %s $OUTPUT_FILE` #Obtengo el tamaño del archivo de log local.	
+		echo "Tamaño maximo del archivo local de log: $TAMANO_MAXIMO , IP del servidor remoto de 			logueo: $IP_SERVIDOR_REMOTO , tamaño del archivo de log local: $TAMANO_ARCHIVO_LOG."
+		exit 0
+		;;
+
+	detener)
+		unset COMANDOS
+		;;
+
+	*)
+		echo "El mensaje $1 no forma parte de la interface."
+		exit 1
+		;;
+esac
+
 
 
 
