86afe2dff7905883356a548b15c27d3518fd666a
pfox182 <pfox.182@gmail.com>
2012-10-16 15:48:30 -0300

    built-in/mostrar.sh -> 	*Implemente mostrar, aunque x ahora muestra solo los valores de variables que sean definidas con export.

---
+++ built-in/mostrar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+VAR=`env | grep -e "^$VAR="`
+VALUE=`env | grep -e "^$1=" | cut -d "=" -f 2` 
+if ( test -n $VAR );then #Si $VAR es distinto de vacio
+	echo $VALUE
+else
+	echo "La variable $1 no se encuentra definida"
+fi
