01a59348a7858a92eec281942d39264f55d0f161
pfox182 <pfox.182@gmail.com>
2012-10-16 13:10:50 -0300

    built-in/salir.sh -> 	*Se cambio el grep shield.sh por $SHIELD

--- built-in/salir.sh
+++ built-in/salir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PID_NUCLEO_DE_SESION=`ps auxh | grep shield.sh | grep $TTY | grep -v grep | awk '{ print $2 }'`
+PID_NUCLEO_DE_SESION=`ps auxh | grep $SHIELD | grep $TTY | grep -v grep | awk '{ print $2 }'`
 kill -15 $PID_NUCLEO_DE_SESION
 exit 0
